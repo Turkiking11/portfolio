6c40dada44d9852e1ffe067ee1931f47bc070781
Turkiking11 <136961973+Turkiking11@users.noreply.github.com>
2026-05-15 13:02:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function animateName() {
+  const name = document.querySelector(".name");
 
+  name.style.transition = "0.5s";
+  name.style.transform = "scale(1.2)";
+
+  setTimeout(() => {
+    name.style.transform = "scale(1)";
+  }, 500);
+}
